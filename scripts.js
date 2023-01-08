b5d2655eaaa4c80de1d0b4824d15537863757aa2
Grzegorz Jóźwiak <grzegorz.jozw@gmail.com>
2023-01-08 13:51:22 +0100

    Init new project

--- scripts.js
+++ scripts.js
@@ -1,24 +1,4 @@
-var h1, prevBgColor;
 
-window.onload = function () {
-  h1 = document.getElementsByTagName("h1")[0];
-  prevBgColor = h1.style.color;
-
-  document.body.onpointermove = function () {
-    h1.innerHTML = "Dajesz tam, dajesz!!!";
-    h1.style.color = "white";
-    document.body.style.backgroundColor = "black";
-
-    console.log("Pointer moving...");
-  };
-
-  letsGo();
+onload = function () {
+  
 };
-
-function letsGo() {
-  setInterval(function () {
-    h1.innerHTML = "Witaj Świecie!";
-    h1.style.color = "black";
-    document.body.style.backgroundColor = prevBgColor;
-  }, 5000);
-}
