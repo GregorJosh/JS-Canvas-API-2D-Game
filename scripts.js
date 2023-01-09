66bc3fbdd5b90b4dc51682b0f950e0d4d5dd740c
Grzegorz Jóźwiak <grzegorz.jozw@gmail.com>
2023-01-09 17:58:23 +0100

    Added character move states

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 class Player {
   state = "idle";
+  states = [];
 
   velocity = {
     x: 0,
@@ -48,38 +49,47 @@ class Player {
   // Metoda Update jest wywoływana po przypisaniu wskaźnika do obiektu gry!
 
   update() {
-    switch (this.state) {
-      case "move left": {
-        break;
-      }
-      case "move right": {
-        break;
-      }
-      case "move up": {
-        break;
-      }
-      case "move down": {
-        break;
-      }
-      default: {
-        break;
-      }
-    }
-
     if (this.game.keys && this.game.keys["w"]) {
-      this.velocity.y -= 1;
+      this.state = "move up";
     }
 
     if (this.game.keys && this.game.keys["s"]) {
-      this.velocity.y += 1;
+      this.state = "move down";
     }
 
     if (this.game.keys && this.game.keys["a"]) {
-      this.velocity.x -= 1;
+      this.state = "move left";
     }
 
     if (this.game.keys && this.game.keys["d"]) {
-      this.velocity.x += 1;
+      this.state = "move right";
+    }
+
+    switch (this.state) {
+      case "move left": {
+        this.image.src = "images/skeleton_walk_left.png";
+        this.velocity.x -= 1;
+        break;
+      }
+      case "move right": {
+        this.image.src = "images/skeleton_walk_right.png";
+        this.velocity.x += 1;
+        break;
+      }
+      case "move up": {
+        this.image.src = "images/skeleton_walk_up.png";
+        this.velocity.y -= 1;
+        break;
+      }
+      case "move down": {
+        this.image.src = "images/skeleton_walk_down.png";
+        this.velocity.y += 1;
+        break;
+      }
+      default: {
+        this.image.src = "images/skeleton_walk_right.png";
+        break;
+      }
     }
 
     this.position.x += this.velocity.x;
@@ -87,6 +97,12 @@ class Player {
 
     this.velocity.x = 0;
     this.velocity.y = 0;
+
+    this.state = "idle";
+  }
+
+  addState(state) {
+
   }
 }
 
@@ -143,7 +159,7 @@ class Game {
 
 window.onload = function () {
   const game = new Game();
-  const player = new Player(10, 10, 28, 48, "images/skeleton.png", "image");
+  const player = new Player(10, 10, 28, 48, "images/skeleton_walk_right.png", "image");
 
   game.addGameObject(player);
   game.start();
